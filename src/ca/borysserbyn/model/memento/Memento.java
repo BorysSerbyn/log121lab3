36b547c708bb2f7c864efc3fab432089e0b216ac
BorysSerbyn <borys.serbyn@gmail.com>
2021-03-31 20:42:15 -0400

    Added comments

---
+++ src/ca/borysserbyn/model/memento/Memento.java
@@ -0,0 +1,4 @@
+package ca.borysserbyn.model.memento;
+
+public class Memento {
+}
--- src/ca/borysserbyn/view/PreviewPanel.java
+++ src/ca/borysserbyn/view/PreviewPanel.java
@@ -8,6 +8,10 @@ import java.awt.image.BufferedImage;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * CETTE CLASSE EST LE BLUEPRINT POUR ZOOMPANEL
+ */
+
 public class PreviewPanel extends JPanel implements Observer {
     private BufferedImage image;
     private JLabel imageLabel;
@@ -24,10 +28,13 @@ public class PreviewPanel extends JPanel implements Observer {
         add(imageLabel);
         imageLabel.setPreferredSize(new Dimension(300, 250));
         setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-
     }
 
     public void update(Observable arg0, Object arg1) {
+        /*
+        TODO: si zoom et translate view sont independant, faire ca:
+        image = ((Originator) arg0).getImageEdit().getZoomedImage();
+         */
         image = ((Originator) arg0).getImageEdit().getImage();
         imageLabel.setIcon(new ImageIcon(image));
     }
